cdbf48cbf7c4d24fa8412f4ec009e785de1957b6
rilwan-p-r <rilwanpr@gmail.com>
2024-03-22 11:32:09 +0530

    Added countAlphabet.js and updated other files

---
+++ string/countAlphabet.js
@@ -0,0 +1,16 @@
+function countAlphabet(value){
+    let result =""
+    let count =1
+    for(let i=0; i<value.length; i++){
+        if(value[i]===value[i+1]){
+            count++
+        }else{
+            result += count + value[i]
+            count = 1
+        }
+    }
+    return result 
+}
+let str = "AAABBC"
+let output = countAlphabet(str)
+console.log(output);
