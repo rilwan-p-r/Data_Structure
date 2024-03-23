3804ac826d9923b37cfb9711c0310daf3f63a18b
rilwan-p-r <rilwanpr@gmail.com>
2024-03-23 12:28:49 +0530

    learned two types of sorting bubbleSort and insertionSort

---
+++ sortings/bubbleSort.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr){
+    let swapped
+    do{
+        swapped=false
+    for(let i=0; i<arr.length-1; i++){
+        if(arr[i]>arr[i+1]){
+            let temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+            swapped = true
+        }
+    }
+}while(swapped)
+}
+let arr = [-20,6,3,-1,0]
+bubbleSort(arr)
+console.log(arr);
+// complexity
+// O(n^2)T
+// O(1)S
